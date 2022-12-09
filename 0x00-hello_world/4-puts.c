b05e0c96e93e2a4b9c9c120a72392114d9c08110
ShehabElgendy <shehabelgendy@hotmail.com>
2022-12-08 17:02:18 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-/***
+
+/**
  * main - Entry point
  *
- * Return: ALways 0 (Sucess)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
