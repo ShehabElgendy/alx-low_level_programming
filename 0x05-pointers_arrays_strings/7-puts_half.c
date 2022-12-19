f4899f4c3132df749e769992865e7626ec1bb57b
ShehabElgendy <shehabelgendy@hotmail.com>
2022-12-19 06:07:54 -0800

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,28 @@
  */
 void puts_half(char *str)
 {
-	int i;
+	int i, length, half;
 
-	for (i = 0; str[i] != '\0'; i++)
-		i++;
-	for (i / = 2; str[i] != '\0; i++)
+	length = 0;
+	while (str[length] != '\0')
 	{
-		_putchar(str[i]);
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		half = length / 2;
+		for (i = half; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		half = (length - 1) / 2;
+		for (i = half + 1; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
