7b00a420a7055caa4c9d1d74798e5bcd933a24e7
ShehabElgendy <shehabelgendy@hotmail.com>
2022-12-23 23:14:03 +0200

    3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,10 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
- * @b: second integer
- * @c: third integer
+ * @b: second integeff * @c: third integer
  * Return: largest number
  */
 
