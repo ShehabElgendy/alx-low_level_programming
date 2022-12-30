6194df4f776c6c8900bd3e8b2b1a4d2fa475dd58
ShehabElgendy <shehabelgendy@hotmail.com>
2022-12-30 17:41:29 +0200

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_square - Print square lines.
+ * @n: Number of lines
+ *
+ * Return: There is no return value
+ */
+void print_square(int size)
+{
+    int i;
+    int n;
+    
+    if (n > 0)
+    {
+        for (i = 0; i <= n; i++);
+        _putchar("#");
+    }
+}
