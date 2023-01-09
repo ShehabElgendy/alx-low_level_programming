06f88cc31b827ff14d41da004e8d99a1b1cead3e
ShehabElgendy <shehabelgendy@hotmail.com>
2023-01-09 05:30:13 -0800

    add

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,16 @@
 #include "main.h"
-
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
+ * *_puts - prints a sting
+ * * @str: string to print
+ *
+ * Description: prints a string
+ * On success: return the number of characters printed
  */
+
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
+	while (*str)
+		_putchar(*str++);
 	_putchar('\n');
 }
 
